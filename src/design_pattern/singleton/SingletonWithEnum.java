304ba0fa08cbb8380c9b5318b5455cd2b81b0590
mehedy22 <mehedy.mmc272@gmail.com>
2026-02-08 02:04:42 +0600

    Add SingletonWithEnum implementation

---
+++ src/design_pattern/singleton/SingletonWithEnum.java
@@ -0,0 +1,12 @@
+package design_pattern.singleton;
+
+/*
+* By Default, all constructors are private, and so enum by default only one it's object is created, and in the
+* So as per JVM each JVM only one snapshot of the enum is present.
+*
+* */
+
+public enum SingletonWithEnum {
+
+    INSTANCE;
+}
