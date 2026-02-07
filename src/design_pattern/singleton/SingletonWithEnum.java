8eced75e3f5a50cd7f530e10d1b3891a1d909200
mehedy22 <mehedy.mmc272@gmail.com>
2026-02-08 02:05:09 +0600

    Update comments in SingletonWithEnum for clarity

--- src/design_pattern/singleton/SingletonWithEnum.java
+++ src/design_pattern/singleton/SingletonWithEnum.java
@@ -2,7 +2,7 @@ package design_pattern.singleton;
 
 /*
 * By Default, all constructors are private, and so enum by default only one it's object is created, and in the
-* So as per JVM each JVM only one snapshot of the enum is present.
+* So as per JVM each JVM only one snapshot of the enum is present. So it's do one object per JVM.
 *
 * */
 
