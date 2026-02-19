69ef78e3ea9e104a63de7be1c8ffa68ac5d524a5
mehedy22 <mehedy.mmc272@gmail.com>
2026-02-19 06:40:29 +0600

    Example of reflection of Class Type

---
+++ src/reflection/classes/Main.java
@@ -0,0 +1,12 @@
+package reflection.classes;
+
+import java.lang.reflect.Modifier;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Class reflectionClass = ReflectionClasses.class;
+        System.out.println(reflectionClass.getName());
+        System.out.println(Modifier.toString(reflectionClass.getModifiers()));
+    }
+}
--- src/reflection/classes/ReflectionClasses.java
+++ src/reflection/classes/ReflectionClasses.java
@@ -1,7 +1,14 @@
 package reflection.classes;
-// Reflection
-
-
 
 public class ReflectionClasses {
+    public String bread;
+    private boolean canSwim;
+
+    public void fly() {
+        System.out.println("I am flying");
+    }
+
+    public void eat() {
+        System.out.println("I am eating");
+    }
 }
