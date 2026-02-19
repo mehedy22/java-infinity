cb58ed82b81504023148893ae8d9a64525113810
mehedy22 <mehedy.mmc272@gmail.com>
2026-02-19 06:56:33 +0600

    Example of Reflection With Fields Type

--- src/reflection/fields/Main.java
+++ src/reflection/fields/Main.java
@@ -2,13 +2,24 @@ package reflection.fields;
 
 import reflection.classes.ReflectionClasses;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
 public class Main {
 
     public static void main(String[] args) {
-        Class reflectionClass = ReflectionClasses.class;
-        System.out.println(reflectionClass.getName());
-        System.out.println(Modifier.toString(reflectionClass.getModifiers()));
+        Class reflectionFields = ReflectionFields.class;
+
+        // get public fields
+        Field[] fields = reflectionFields.getFields();
+        // get private fields
+        Field[] fieldsPrivates = reflectionFields.getDeclaredFields();
+
+        for (Field field : fields) {
+            System.out.println("Field name: " + field.getName());
+            System.out.println("Field type: " + field.getType());
+            System.out.println("Modifier: " + Modifier.toString(field.getModifiers()));
+        }
+
     }
 }
---
+++ src/reflection/fields/ReflectionFields.java
@@ -0,0 +1,14 @@
+package reflection.fields;
+
+public class ReflectionFields {
+    public String bread;
+    private boolean canSwim;
+
+    public void fly() {
+        System.out.println("I am flying");
+    }
+
+    public void eat() {
+        System.out.println("I am eating");
+    }
+}
