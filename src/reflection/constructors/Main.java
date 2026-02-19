e4b5c316f86ce3cc16330703c5284fcc1e239216
mehedy22 <mehedy.mmc272@gmail.com>
2026-02-19 07:02:02 +0600

    Example of Reflection With Constructors Type

--- src/reflection/constructors/Main.java
+++ src/reflection/constructors/Main.java
@@ -1,14 +1,32 @@
 package reflection.constructors;
 
-import reflection.classes.ReflectionClasses;
-
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Class reflectionClass = ReflectionClasses.class;
-        System.out.println(reflectionClass.getName());
-        System.out.println(Modifier.toString(reflectionClass.getModifiers()));
+    public static void main(String[] args) throws InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class reflectionConstructors = ReflectionConstructors.class;
+
+        // get private constructor
+        Constructor[] constructors = reflectionConstructors.getDeclaredConstructors();
+        for (Constructor constructor : constructors) {
+            System.out.println("Modifier: " + Modifier.toString(constructor.getModifiers()) );
+            System.out.println("Constructor name: " + constructor.getName());
+
+
+            /*
+            * Here one of the major concern is Reflection break the concept of Singleton here
+            * IN this example we have seen that even the Constructor is Private with Reflection
+            * we still can create an object of that class.
+            *
+            * */
+            constructor.setAccessible(true);
+            ReflectionConstructors object=(ReflectionConstructors) constructor.newInstance();
+            object.eat();
+            object.fly();
+        }
+
     }
 }
--- src/reflection/constructors/ReflectionConstructors.java
+++ src/reflection/constructors/ReflectionConstructors.java
@@ -1,4 +1,18 @@
 package reflection.constructors;
 
 public class ReflectionConstructors {
+    public String bread;
+    private boolean canSwim;
+
+    private ReflectionConstructors() {
+
+    }
+
+    public void fly() {
+        System.out.println("I am flying");
+    }
+
+    public void eat() {
+        System.out.println("I am eating");
+    }
 }
